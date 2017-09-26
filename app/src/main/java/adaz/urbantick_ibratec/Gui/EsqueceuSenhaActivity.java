65486e0073f14fde854bd2a35eb40ec7ecf5337b
jessicalb <jessica.alb@hotmail.com>
2017-09-26 07:07:39 -0300

    Corregido o back da tela EsqueceuSenha para Main

--- app/src/main/java/adaz/urbantick_ibratec/Gui/EsqueceuSenhaActivity.java
+++ app/src/main/java/adaz/urbantick_ibratec/Gui/EsqueceuSenhaActivity.java
@@ -3,12 +3,10 @@ package adaz.urbantick_ibratec.Gui;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import adaz.urbantick_ibratec.Model.Usuario;
@@ -72,5 +70,11 @@ public class EsqueceuSenhaActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(EsqueceuSenhaActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
